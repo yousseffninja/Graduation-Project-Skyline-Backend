4592e11158b37310207fc4c1baa69d31bc5b3fb5
backm <yousseffmohamedd22@gmail.com>
2023-04-18 22:39:19 +0200

    Integrate booking with mobile phone

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -41,7 +41,7 @@ exports.getCheckoutSessionFlight = catchAsync(async (req, res, next) => {
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/api/v1/bookings/success/${req.params.flightId}/${req.params.seatID}`,
+    success_url: `exp://192.168.1.4:19000/--/Home?flightId=${req.params.flightId}&seatID=${req.params.seatID}`,
     cancel_url: `${req.protocol}://${req.get('host')}/flight`,
     customer_email: req.user.email,
     client_reference_id: req.params.flightId,
