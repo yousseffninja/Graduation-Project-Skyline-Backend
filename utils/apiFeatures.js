3364825433e9d47d7b99df494acea5c771fae108
backm <yousseffmohamedd22@gmail.com>
2023-06-15 11:48:59 +0300

    fix error

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -11,7 +11,7 @@ class APIFeatures {
 
         // 1B) Advanced filtering
         let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt|in)\b/g, match => `$${match}`);
 
         this.query = this.query.find(JSON.parse(queryStr));
 
